46e633d73359fa6745c92ca87b3efd75300afc64
stsparctice <practicestuds@gmail.com>
2023-10-22 11:34:28 +0300

    add tables

--- modules/config/project.config.js
+++ modules/config/project.config.js
@@ -14,62 +14,62 @@ const getAllDBConfig = ()=>{
 module.exports = {getDBConfig, getAllDBConfig}
 
 
-function buildJoinAndSelect2(extra){
-    function getSelectedColumns(myTable, entityFields) {
-        const { entity, fields } = entityFields
-        const selectColumns = myTable.columns.filter(col => fields.includes(col.name))
-        return selectColumns
+// function buildJoinAndSelect2(extra){
+//     function getSelectedColumns(myTable, entityFields) {
+//         const { entity, fields } = entityFields
+//         const selectColumns = myTable.columns.filter(col => fields.includes(col.name))
+//         return selectColumns
     
-    }
+//     }
     
-    function getJoinColumn(myTable, joinTable) {
-        const allForeignKeys = joinTable.columns.filter(col => col.type.includes('FOREIGN KEY'))
-        const foreignkey = allForeignKeys.find(col => col.type.includes(myTable.MTDTable.entityName.sqlName))
-        return foreignkey
-    }
+//     function getJoinColumn(myTable, joinTable) {
+//         const allForeignKeys = joinTable.columns.filter(col => col.type.includes('FOREIGN KEY'))
+//         const foreignkey = allForeignKeys.find(col => col.type.includes(myTable.MTDTable.entityName.sqlName))
+//         return foreignkey
+//     }
 
-    const myTable = getTableFromConfig(configUrl, tableName)
-    const tableAlias = getTableAlias(configUrl, tableName)
-    // const columns = myTable.columns.filter(({ type }) => type.toLowerCase().includes('foreign key'));
-    // let columnsSelect = [{ tableName: myTable.MTDTable.entityName.name, columnsName: [...myTable.columns.map(({ sqlName, name }) => ({ sqlName, name }))] }];
-    let selectColumns = []
-    if (entitiesFields) {
-        const mainSelectedEntity = entitiesFields.find(e => e.entity.toLowerCase() === tableName.toLowerCase())
-        if (mainSelectedEntity) {
-            let cols = getSelectedColumns(myTable, mainSelectedEntity)
-            selectColumns = [...cols.map(({ sqlName, name }) => ({ sqlName, name , alias:tableAlias}))]
-        }
-    }
-    else {
-        selectColumns = [...myTable.columns.map(({ sqlName, name }) => ({ sqlName, name }))]
-    }
-    let join = `${myTable.MTDTable.entityName.sqlName} ${myTable.MTDTable.entityName.name}`;
-    entitiesFields.filter(ef => ef.entity.toLowerCase() !== tableName).forEach(
-        ({ entity, fields }) => {
-            const joinTable = getTableFromConfig(configUrl, entity)
-          const joinColumn =   getJoinColumn(myTable, joinTable)
-          const joinAlias = getTableAlias(configUrl, entity)
-          let cols = getSelectedColumns(joinTable, fields)
-          selectColumns = [...selectColumns,...cols.map(({ sqlName, name }) => ({ sqlName, name , alias:joinAlias}))] 
-          join = `${join} LEFT JOIN ${tableToJoin} ${joinAlias} ON ${tableAlias}.${column.sqlName}=${alias}.${columnToJoin}`;
+//     const myTable = getTableFromConfig(configUrl, tableName)
+//     const tableAlias = getTableAlias(configUrl, tableName)
+//     // const columns = myTable.columns.filter(({ type }) => type.toLowerCase().includes('foreign key'));
+//     // let columnsSelect = [{ tableName: myTable.MTDTable.entityName.name, columnsName: [...myTable.columns.map(({ sqlName, name }) => ({ sqlName, name }))] }];
+//     let selectColumns = []
+//     if (entitiesFields) {
+//         const mainSelectedEntity = entitiesFields.find(e => e.entity.toLowerCase() === tableName.toLowerCase())
+//         if (mainSelectedEntity) {
+//             let cols = getSelectedColumns(myTable, mainSelectedEntity)
+//             selectColumns = [...cols.map(({ sqlName, name }) => ({ sqlName, name , alias:tableAlias}))]
+//         }
+//     }
+//     else {
+//         selectColumns = [...myTable.columns.map(({ sqlName, name }) => ({ sqlName, name }))]
+//     }
+//     let join = `${myTable.MTDTable.entityName.sqlName} ${myTable.MTDTable.entityName.name}`;
+//     entitiesFields.filter(ef => ef.entity.toLowerCase() !== tableName).forEach(
+//         ({ entity, fields }) => {
+//             const joinTable = getTableFromConfig(configUrl, entity)
+//           const joinColumn =   getJoinColumn(myTable, joinTable)
+//           const joinAlias = getTableAlias(configUrl, entity)
+//           let cols = getSelectedColumns(joinTable, fields)
+//           selectColumns = [...selectColumns,...cols.map(({ sqlName, name }) => ({ sqlName, name , alias:joinAlias}))] 
+//           join = `${join} LEFT JOIN ${tableToJoin} ${joinAlias} ON ${tableAlias}.${column.sqlName}=${alias}.${columnToJoin}`;
 
-        }
-    )
-    // columns.forEach(column => {
-    //     const tableToJoin = column.type.slice(column.type.lastIndexOf('tbl_'), column.type.lastIndexOf('('));
-    //     const columnToJoin = column.type.slice(column.type.lastIndexOf('(') + 1, column.type.lastIndexOf(')'));
-    //     const thisTable = getTableFromConfig(configUrl, tableToJoin);
-    //     const alias = thisTable.MTDTable.entityName.name;
-    //     columnsSelect = [...selectColumns, { tableName: alias, columnsName: [`${columnToJoin} as FK_${column.name}_${columnToJoin}`, `${thisTable.MTDTable.defaultColumn} as FK_${column.name}_${thisTable.MTDTable.defaultColumn}`] }];
-    //     join = `${join} LEFT JOIN ${tableToJoin} ${alias} ON ${myTable.MTDTable.entityName.name}.${column.sqlName}=${alias}.${columnToJoin}`;
-    // });
+//         }
+//     )
+//     // columns.forEach(column => {
+//     //     const tableToJoin = column.type.slice(column.type.lastIndexOf('tbl_'), column.type.lastIndexOf('('));
+//     //     const columnToJoin = column.type.slice(column.type.lastIndexOf('(') + 1, column.type.lastIndexOf(')'));
+//     //     const thisTable = getTableFromConfig(configUrl, tableToJoin);
+//     //     const alias = thisTable.MTDTable.entityName.name;
+//     //     columnsSelect = [...selectColumns, { tableName: alias, columnsName: [`${columnToJoin} as FK_${column.name}_${columnToJoin}`, `${thisTable.MTDTable.defaultColumn} as FK_${column.name}_${thisTable.MTDTable.defaultColumn}`] }];
+//     //     join = `${join} LEFT JOIN ${tableToJoin} ${alias} ON ${myTable.MTDTable.entityName.name}.${column.sqlName}=${alias}.${columnToJoin}`;
+//     // });
 
-    let select = selectColumns.map(({alias, sqlName, name}) => `${alias}.${sqlName} as ${name}`);
+//     let select = selectColumns.map(({alias, sqlName, name}) => `${alias}.${sqlName} as ${name}`);
 
-    select = select.join(', ');
+//     select = select.join(', ');
 
-    return `SELECT ${select} FROM ${join}`
+//     return `SELECT ${select} FROM ${join}`
     
-}
+// }
 
 
